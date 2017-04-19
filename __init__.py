6c7cc28d1bd147a6cf7203a9a831db5eed941831
swift-n-brutal <swift-n-brutal@users.noreply.github.com>
2017-04-19 15:43:39 +0800

    Update __Init__
    Add new classes.

--- __init__.py
+++ __init__.py
@@ -8,17 +8,22 @@ Created on Fri Nov 11 15:13:43 2016
 from .config import USE_GPU, DEVICE_ID, PYCAFFE_ROOT
 #import sys
 #sys.path.insert(0, PYCAFFE_ROOT)
+from caffe import TRAIN, TEST
 if USE_GPU:
     from .util import get_block_shape, get_grid_shape, set_device, CAFFE_CUDA_CONTEXT
     print "Do not use caffe.set_mode_cpu()/set_mode_gpu()/set_device()"
     print "Use latte.set_device() instead"
     from . import math_func
 from .solver import SGDSolver
+from .solver_all import Solver
+from .solver_wgan_vae import SolverWGANVAE
 from .softrelu_solver import SoftReLUSolver
 from .newton_solver import NewtonSolver
 from .solver_wgan import SolverWGAN
+from .solver_boundary_equilibrium_gan import SolverBoundaryEquilibriumGAN
 from .net import Net
 from .blob import Blob
 from .dataloader import CifarDataLoader, CifarTransformer, CifarDataLoaderMultiThreading
-from .image_dataloader import ImageDataLoader, ImageDataLoaderPrefetch, ImageTransformer
+from .image_dataloader import ImageDataLoader, ImageDataLoaderPrefetch, \
+        ImageTransformer, BBoxImageDataLoaderPrefetch, CelebADataLoaderPrefetch
 from .rand_dataloader import RandDataLoader
